1f335d98633bbda4e22d506befdd3ccbdf38b0ba
arifibnenam <arifibnenam@gmail.com>
2022-05-11 12:11:28 +0600

    header section added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './pages/shared/Header/Header';
+import Footer from './pages/shared/Footer/Footer';
+import Home from './pages/Home/Home/Home';
+import Appointment from './pages/Appointment/Appointment';
+import About from './pages/About/About';
+import ContactUs from './pages/ContactUs/ContactUs';
+import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
+import NotFound from './pages/NotFound/NotFound';
+import Reviews from './pages/Reviews/Reviews';
+import { Route, Routes } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/appointment' element={<Appointment></Appointment>}></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/contact-us' element={<ContactUs></ContactUs>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
+      <Footer></Footer>
+    </>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "rgba(15, 207, 236, 1)",
+          secondary: "rgba(25, 211, 174, 1)",
+          accent: "rgba(58, 66, 86, 1)",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
